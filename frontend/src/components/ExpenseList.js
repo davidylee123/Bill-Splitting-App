814c88f406bea75134da0742899f36020cb02602
flannelmn <114701863+flannelmn@users.noreply.github.com>
2024-10-20 14:02:33 -0400

    Format expenses in mui table

--- frontend/src/components/ExpenseList.js
+++ frontend/src/components/ExpenseList.js
@@ -1,8 +1,46 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+
+const columns = [
+    { id: 'title', label: 'Title', minWidth: 170 },
+    { id: 'amount', label: 'Amount', minWidth: 100 },
+    {
+      id: 'user',
+      label: 'Paid By',
+      minWidth: 170,
+      align: 'right',
+    },
+  ];
+
+
+
+
 
 const ExpenseList = () => {
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(+event.target.value);
+        setPage(0);
+    };
+
+
+
+
     const [Expenses, setExpenses] = useState([]);
     const [title, setTitle] = useState('');
     const [amount, setAmount] = useState('');
@@ -62,20 +100,67 @@ const ExpenseList = () => {
             </form>
 
 
-            <h1>Expenses</h1>
-            <ul>
-                {Expenses.map(Expense => (
-                    <li key={Expense.id}>
-                        {Expense.title}
-                        ___
-                        {Expense.amount}
-                        ___
-                        {Expense.user}
-                    </li>
-                ))}
-            </ul>
+{/*testing MUI component*/}
+
+
+            <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+      <TableContainer sx={{ maxHeight: 440 }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+                    <TableCell colSpan={3} align="center"><h2>Expenses</h2></TableCell>
+            </TableRow>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {Expenses
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((Expenses) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={Expenses.code}>
+                    {columns.map((column) => {
+                      const value = Expenses[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.format && typeof value === 'number'
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={Expenses.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+
+
+
+
         </div>
     );
+
 };
 
 export default ExpenseList;
