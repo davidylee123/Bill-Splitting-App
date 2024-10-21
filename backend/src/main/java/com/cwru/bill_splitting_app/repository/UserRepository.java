bfd375fddc4dfcb39382341cd997ca02e714b184
Zinput <jasonzheng2004@gmail.com>
2024-10-21 18:19:09 -0400

    Added stubs for unit testing and removed authentication (for now) to facilitate unit testing and faster development.

--- backend/src/main/java/com/cwru/bill_splitting_app/repository/UserRepository.java
+++ backend/src/main/java/com/cwru/bill_splitting_app/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.cwru.bill_splitting_app.repository;
 
 import com.cwru.bill_splitting_app.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends MongoRepository<User, String> {
 }
