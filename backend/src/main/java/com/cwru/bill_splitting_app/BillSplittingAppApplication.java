f14a241d7ba29df4f0eaec4879e486aee652dcbf
flannelmn <114701863+flannelmn@users.noreply.github.com>
2024-12-01 01:39:47 -0500

    Delete working.

--- backend/src/main/java/com/cwru/bill_splitting_app/BillSplittingAppApplication.java
+++ backend/src/main/java/com/cwru/bill_splitting_app/BillSplittingAppApplication.java
@@ -3,6 +3,7 @@ package com.cwru.bill_splitting_app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,9 +18,14 @@ public class BillSplittingAppApplication {
   public WebMvcConfigurer corsConfigurer() {
     return new WebMvcConfigurer() {
       @Override
-      public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000");
-      }
+      public void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+                registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:3000")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE")
+                    .allowedHeaders("*")
+                    .allowCredentials(true);
+               }
     };
   }
 }
