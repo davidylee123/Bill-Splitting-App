9414ff3251bf5c5e68528db2beab2a9bae347d5c
Zinput <jasonzheng2004@gmail.com>
2024-12-06 23:59:07 -0500

    allow all origins for hosting

--- backend/src/main/java/com/cwru/bill_splitting_app/BillSplittingAppApplication.java
+++ backend/src/main/java/com/cwru/bill_splitting_app/BillSplittingAppApplication.java
@@ -20,7 +20,7 @@ public class BillSplittingAppApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
